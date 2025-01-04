130110575c2565040752b0703dac2ce46d65e571
BugHunter-X <askzenai@gmail.com>
2025-01-04 09:02:06 +0100

    Add solution.py file

---
+++ solution.py
@@ -0,0 +1,5 @@
+def function_with_uncommon_error_solution(x):
+    if x == 0:
+        return float('inf') # Or handle it in a more meaningful way for your application.
+    else:
+        return x + 5
